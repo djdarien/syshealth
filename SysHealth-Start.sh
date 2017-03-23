5c05465be35ed1199b1a7c47a84ffa188754cd5c
Darien <junk@netbyteservers.net>
2017-03-22 21:27:27 -0400

    Update SysHealth-Start.sh

--- SysHealth-Start.sh
+++ SysHealth-Start.sh
@@ -2,7 +2,7 @@
 now=$(date +"%m_%d_%Y")
 echo "This script requires SUDO access to run! Please allow!"
 echo "Once the script is complete the syshelth script will place a report txt file in your users home directory."
-sudo sh ./syshealth.sh > ./system_health_reportnow=$.txt
+sudo sh ./syshealth.sh > ./system_health_reportnow$.txt
 echo "......."
 echo "..........."
 echo "..............."
