24f204c95332a9868b3bbf608bdca87e27670f06
Darien <djentwistle@DarienEntwistle-VMBP.local>
2018-07-31 13:11:29 -0400

    added hostname to distinguish report & fixed attachment

--- syshealth-mailer.sh
+++ syshealth-mailer.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 now=$(date +"%m_%d_%Y")
+
 sudo sh ./syshealth.sh > ./system_health_report$now.txt
-echo "Hello! Your System health report for $now is ready and has been attached to this email. Have a great day!" | mail -s "System Health Report for $now" dentwistle@voalte.com -A system_health_report$now.txt
+sleep 15s
+echo "Hello! Your System health report for $hostname of date $now is ready and has been attached to this email. Have a great day!" | mail -s "System Health Report for $now" dentwistle@voalte.com -a $hostname-system_health_report$now.txt
