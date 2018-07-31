54d46fdddf1f0074b96e48ae09cff826f0de4aac
Darien <djentwistle@DarienEntwistle-VMBP.local>
2018-07-31 12:52:47 -0400

    fixed typo

--- syshealth-mailer.sh
+++ syshealth-mailer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 now=$(date +"%m_%d_%Y")
 sudo sh ./syshealth.sh > ./system_health_report$now.txt
-wecho "Hello! Your System health report for $now is ready and has been attached to this email. Have a great day!" | mail -s "System Health Report for $now" dentwistle@voalte.com -A system_health_report$now.txt 
+echo "Hello! Your System health report for $now is ready and has been attached to this email. Have a great day!" | mail -s "System Health Report for $now" dentwistle@voalte.com -A system_health_report$now.txt
