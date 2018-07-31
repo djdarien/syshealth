96bbf32b907463ddadf0c77101d26886be7f5cc1
Darien <djentwistle@gmail.com>
2018-07-30 20:01:35 -0400

    lets try this

--- syshealth-mailer.sh
+++ syshealth-mailer.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 now=$(date +"%m_%d_%Y")
-echo "Hello! Your System health report for $now is ready and has been attached to this email. Have a great day!" | mail -s "System Health Report for $now" djdarien@protonmail.com -A sh ./syshealth.sh > ./system_health_report$now.txt
+sudo sh ./syshealth.sh > ./system_health_report$now.txt echo "Hello! Your System health report for $now is ready and has been attached to this email. Have a great day!" | mail -s "System Health Report for $now" djdarien@protonmail.com -A system_health_report$now.txt 
