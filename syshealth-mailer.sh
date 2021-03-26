7a48e61eb1b2bdc78d75f297d0fa508a9a6400a1
Darien <djentwistle@gmail.com>
2021-03-25 23:51:03 -0400

    Update syshealth-mailer.sh

--- syshealth-mailer.sh
+++ syshealth-mailer.sh
@@ -3,4 +3,4 @@ now=$(date +"%m_%d_%Y")
 servername= hostname
 sudo sh ./syshealth.sh > ./system_health_report$now.txt
 sleep 5s
-echo "Hello! Your System health report for $servername of date $now is ready and has been attached to this email. Have a great day!" | mailx -s "System Health Report for $now "  -a system_health_report$now.txt dentwistle@voalte.com
+echo "Hello! Your System health report for $servername of date $now is ready and has been attached to this email. Have a great day!" | mailx -s "System Health Report for $now "  -a system_health_report$now.txt emailhere@dddomain.com
